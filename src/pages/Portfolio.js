7d6373bc531c5fa40556ebff384ddb9c92d9f900
LovepreetSingh24 <lovepreet.singh@att.net>
2024-03-02 21:21:12 -0800

    Update Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
-import Project from '../components/Project';
 
 const projects = [
-  { title: "Project 1", image: "path/to/image", deployedLink: "#", repoLink: "#" },
+  {
+    title: "Project 1",
+    image: "path/to/image",
+    deployedLink: "#",
+    repoLink: "#",
+  },
 ];
 
 function Portfolio() {
   return (
     <Container>
       <h2 className="mt-5">Portfolio</h2>
-      <Row xs={1} md={2} className="g-4">
+      <Row xs={1} md={3} className="g-4">
         {projects.map((project, idx) => (
           <Col key={idx}>
-            <Project {...project} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-}
-
-export default Portfolio;
+            <Card>
+              <Card.Img variant="top" src={project.image} />
+              <Card.Body>
+                <Card.Title>{project.title}</Card.Title>
+                <Button variant="primary" href={project.deployedLink}>View Project</Button>
+                <Button variant="secondary" href={project.repoLink} className="ms-2">GitHub
